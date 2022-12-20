af2be55c490a497ffcae68c8e856d912aa24526d
Alexandr Bezkorovajnyj <alexkaravay@gmail.com>
2022-12-20 16:44:21 +0400

    Util

--- Pre1-1-4/src/main/java/jm/task/core/jdbc/Main.java
+++ Pre1-1-4/src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private final static UserServiceTest userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Александр", "Иванович", (byte) 45);
+        userService.saveUser("Людмила", "Викторовна", (byte) 42);
+        userService.saveUser("Ярослав", "Александрович", (byte) 11);
+        userService.saveUser("Алина", "Шамильевна", (byte) 16);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
--- Pre1-1-4/src/main/java/jm/task/core/jdbc/util/Util.java
+++ Pre1-1-4/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String HOST = "jdbc:mysql://localhost:3306/test?useSSL=false&allowMultiQueries=true&serverTimezone=UTC";
+    private static final String LOGIN = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(HOST, LOGIN, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
